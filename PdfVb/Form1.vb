901279837bdd50577e301d2691533c5c1feb8466
Fajar Maulana <fjrmaulana@yahoo.com>
2022-06-15 09:39:23 +0700

    Add project files.

---
+++ PdfVb/Form1.vb
@@ -0,0 +1,15 @@
+﻿Option Explicit On
+Option Strict Off
+
+Imports MigraDoc.DocumentObjectModel.Style
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("hello", vbInformation)
+    End Sub
+End Class
